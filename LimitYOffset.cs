c8ec330b193f98165b748ee8b811eb1744e8ee47
RafaelDavidEspinosaSegura <rafael.espinosa@opitech.com.co>
2026-05-06 10:57:16 -0500

    refactor(Taller2SQL LimitYOffset)

---
+++ LimitYOffset.cs
@@ -0,0 +1,13 @@
+select*
+from doctor d limit 3
+
+
+select*
+from doctor d  limit 3 offset 3
+
+
+select*
+from medicalappointment m limit 2 
+
+select*
+from medicalappointment m limit 2  offset 3
